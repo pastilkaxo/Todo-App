2394a813cce1474ff88a05ff94e262c80688af75
pastilkaxo <vlad.lemeshok@gmail.com>
2023-07-22 18:48:03 +0300

    Update

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,26 +1,69 @@
-import React from "react";
+import React,{Component} from "react";
 import './style/Items.css'
 
-export default function TodoListItem({task,important=false}) {
-          const style ={
-            color: important ? "steelblue" : "black"
+export default class TodoListItem extends Component {
+  
+constructor(){
+  super();
+  this.state = {
+       done:false,
+       important:false,
+      }
+    };
+
+   
+     onTaskClick = () => {
+     this.setState(({done}) => { 
+      return{
+        done:!done
+      }
+     })
+      }
+
+    onMarkImportant =() => {
+      this.setState(({important}) => {
+        return{
+          important:!important
+        }
+      })
+    }
+
+  render(){
+   const  {task} = this.props;
+   const {done,important} = this.state;
+        let classNames = "list-item-task";
+            if(done) {
+              classNames += " text-decoration-line-through"
+            }
+          
+          if(important){
+            classNames += " important"
           }
+    
     return(
-         <div className="list">
-          <span className="list-item-task" style={style}>{task}</span>
-          <span className="d-flex">
-          <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
-      </button>
+      <div className="list">
+      <span className={classNames} onClick={this.onTaskClick}>{task}</span>
+      <span className="d-flex">
+      <button type="button"
+          className="btn btn-outline-success btn-sm float-right"
+          onClick={this.onMarkImportant}
+          >
+    <i className="fa fa-exclamation" />
+  </button>
 
-      <button type="button" className="btn btn-outline-danger btn-sm float-right">
-      &times;
-      </button>
-          </span>
-         </div>
-
-     
-
+  <button type="button" className="btn btn-outline-danger btn-sm float-right">
+  &times;
+  </button>
+      </span>
+     </div>
     )
+  }
 }
+
+
+
+
+
+
+
+
